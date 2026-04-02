5b11f075386a51377c04aae73fefecbd745deeac
wanglg77 <wanglg77@gmail.com>
2026-04-02 12:47:35 +0800

    更新基层质量三年改善,几个行业标准,纠纷案例

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -41,9 +41,25 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
+    Component.ConditionalRender({
+      component: Component.Graph({
+        localGraph: {
+          depth: -1,
+        },
+      }),
+      condition: (page) => page.fileData.slug === "index",
+    }),
+    Component.ConditionalRender({
+      component: Component.Graph(),
+      condition: (page) => page.fileData.slug !== "index",
+    }),
     Component.DesktopOnly(Component.Donate()), // 改成这一行
     Component.DesktopOnly(Component.TableOfContents()),
+    Component.DesktopOnly(Component.RecentNotes({
+      title: "最新更新",
+      limit: 5,
+      showTags: false,
+    })),
     Component.Backlinks(),
   ],
 }
